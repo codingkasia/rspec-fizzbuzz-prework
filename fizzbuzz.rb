738cf8d19de9c3fc3ff7059ed1ad450847784a3f
KATARZYNA ROSENBERG <codingkasia@gmail.com>
2018-03-28 18:55:32 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(num)
   if   (num % 3 == 0) && (num % 5 == 0)
-    return "Fizz"
+    return "FizzBuzz"
   elsif num % 5 == 0 
   return "Buzz"
   elsif num % 3 == 0
-  return "FizzBuzz"
+  return "Fizz"
 else
   return nil
 end
